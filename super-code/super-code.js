c14575d204f2df63c5a06d2df796c468da6fd340
Fred K. Schott <fschott@google.com>
2016-05-18 16:39:14 -0700

    super-code is now polymer!

--- super-code/super-code.js
+++ super-code/super-code.js
@@ -1,3 +1,163 @@
+// http://therelentlessfrontend.com/2010/05/10/javascript-trim-left-trim-right-trim-functions/
+function rtrim(stringToTrim) {
+  return stringToTrim.replace(/\s+$/,'');
+}
+
+// I needed the opposite function today, so adding here too:
+function htmlEncode(value){
+  return String(value)
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;/g, '&');
+}
+
+// I needed the opposite function today, so adding here too:
+function htmlDecode(value){
+  return String(value)
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;/g, '&');
+}
+
+function loadCodeAjax(pre, callback) {
+  var Extensions = {
+    'js': 'javascript',
+    'html': 'markup',
+    'svg': 'markup',
+    'xml': 'markup',
+    'py': 'python',
+    'rb': 'ruby',
+    'ps1': 'powershell',
+    'psm1': 'powershell'
+  };
+
+  var src = pre.getAttribute('data-src');
+  var code = pre.querySelector('code');
+
+  var language, parent = pre;
+  var lang = /\blang(?:uage)?-(?!\*)(\w+)\b/i;
+  while (parent && !lang.test(parent.className)) {
+    parent = parent.parentNode;
+  }
+
+  if (parent) {
+    language = (pre.className.match(lang) || [, ''])[1];
+  }
+
+  if (!language) {
+    var extension = (src.match(/\.(\w+)$/) || [, ''])[1];
+    language = Extensions[extension] || extension;
+  }
+
+  code.className = 'language-' + language;
+
+  code.textContent = 'Loading...';
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', src, true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+
+      if (xhr.status < 400 && xhr.responseText) {
+        code.textContent = xhr.responseText;
+
+        callback();
+      }
+      else if (xhr.status >= 400) {
+        code.textContent = '✖ Error ' + xhr.status + ' while fetching file: ' + xhr.statusText;
+      }
+      else {
+        code.textContent = '✖ Error: File does not exist or is empty';
+      }
+    }
+  };
+
+  xhr.send(null);
+}
+
+
+
+
+Polymer({
+  is: 'super-code',
+  properties: {},
+
+  created: function() {
+    console.log(this.localName + '#' + this.id + ' was created');
+  },
+
+  ready: function() {
+    console.log(this.localName + '#' + this.id + ' has local DOM initialized');
+    console.log(this);
+    var self = this;
+    var myPreEl = this.$$('pre');
+    var myCodeEl = myPreEl.querySelector('code');
+    var headerEl = this.$$('.header');
+    console.log(myPreEl, myCodeEl, headerEl);
+
+    myCodeEl.className += ' language-' + this.getAttribute('language');
+
+    if (this.getAttribute('line-numbers')) {
+      myPreEl.className += ' line-numbers';
+    }
+
+    if (this.getAttribute('highlight-lines')) {
+      myPreEl.dataset.line = this.getAttribute('highlight-lines');
+    }
+
+    if (this.getAttribute('title')) {
+      headerEl.style.display = 'block';
+      headerEl.textContent = this.getAttribute('title');
+    }
+
+    if (this.getAttribute('header-color')) {
+      headerEl.style.backgroundColor = this.getAttribute('header-color');
+    }
+
+    this.hideHeader = () => headerEl.style.display = 'none';
+
+    function paintElement() {
+      self.code = htmlEncode(myCodeEl.innerHTML);
+      Prism.highlightElement(myCodeEl, false);
+    }
+
+    if (this.getAttribute('code-src')) {
+      myPreEl.dataset.src = this.getAttribute('code-src');
+      loadCodeAjax(myPreEl, paintElement);
+      return;
+    }
+
+    if (this.getAttribute('code')) {
+      myCodeEl.innerHTML = this.getAttribute('code');
+    } else if (this.children[0] instanceof HTMLTemplateElement) {
+      myCodeEl.textContent = this.innerHTML;
+    } else {
+      myCodeEl.textContent = this.innerHTML;
+      // myCodeEl.innerHTML = this.innerHTML;
+    }
+
+    setTimeout(paintElement, 0);
+  },
+
+  attached: function() {
+    console.log(this.localName + '#' + this.id + ' was attached');
+  },
+
+  detached: function() {
+    console.log(this.localName + '#' + this.id + ' was detached');
+  },
+
+});
+
+
+
+
 (function() {
   var importDoc = document.currentScript.ownerDocument; // importee
 
@@ -5,89 +165,6 @@
   // that uses Shadow DOM and a template.
   var SuperCodeElement = Object.create(HTMLElement.prototype);
 
-  // http://therelentlessfrontend.com/2010/05/10/javascript-trim-left-trim-right-trim-functions/
-  function rtrim(stringToTrim) {
-      return stringToTrim.replace(/\s+$/,'');
-  }
-
-  // I needed the opposite function today, so adding here too:
-  function htmlEncode(value){
-      return String(value)
-          .replace(/&quot;/g, '"')
-          .replace(/&#39;/g, "'")
-          .replace(/&lt;/g, '<')
-          .replace(/&gt;/g, '>')
-          .replace(/&amp;/g, '&');
-  }
-
-  // I needed the opposite function today, so adding here too:
-  function htmlDecode(value){
-      return String(value)
-          .replace(/&quot;/g, '"')
-          .replace(/&#39;/g, "'")
-          .replace(/&lt;/g, '<')
-          .replace(/&gt;/g, '>')
-          .replace(/&amp;/g, '&');
-  }
-
-  function loadCodeAjax(pre, callback) {
-
-    var Extensions = {
-      'js': 'javascript',
-      'html': 'markup',
-      'svg': 'markup',
-      'xml': 'markup',
-      'py': 'python',
-      'rb': 'ruby',
-      'ps1': 'powershell',
-      'psm1': 'powershell'
-    };
-
-    var src = pre.getAttribute('data-src');
-    var code = pre.querySelector('code');
-
-    var language, parent = pre;
-    var lang = /\blang(?:uage)?-(?!\*)(\w+)\b/i;
-    while (parent && !lang.test(parent.className)) {
-      parent = parent.parentNode;
-    }
-
-    if (parent) {
-      language = (pre.className.match(lang) || [, ''])[1];
-    }
-
-    if (!language) {
-      var extension = (src.match(/\.(\w+)$/) || [, ''])[1];
-      language = Extensions[extension] || extension;
-    }
-
-    code.className = 'language-' + language;
-
-    code.textContent = 'Loading...';
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', src, true);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-
-        if (xhr.status < 400 && xhr.responseText) {
-          code.textContent = xhr.responseText;
-
-          callback();
-        }
-        else if (xhr.status >= 400) {
-          code.textContent = '✖ Error ' + xhr.status + ' while fetching file: ' + xhr.statusText;
-        }
-        else {
-          code.textContent = '✖ Error: File does not exist or is empty';
-        }
-      }
-    };
-
-    xhr.send(null);
-  }
 
 
   SuperCodeElement.createdCallback = function() {
@@ -162,5 +239,5 @@
     prototype: SuperCodeElement
   });
 
-})();
+});
 
